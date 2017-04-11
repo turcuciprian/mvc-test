8ec68fe2d01f7230b4d4f249c450b731bd034d6f
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-11 22:27:17 +0300

    created folders model, view controller added a swift file Person in tbe model folder, started creating variables - private and public

---
+++ mvc-test/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  mvc-test
+//
+//  Created by Turcu Ciprian on 11/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import Foundation
+
+class Person{
+    private var _firstName: String
+    private var _lastName: String
+    var firstName:String{
+        return _firstName
+    }
+}
