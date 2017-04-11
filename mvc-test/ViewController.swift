f70eef7579610afa53fc708d34774a72ee38bcf7
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-11 22:21:06 +0300

    Initial Commit

---
+++ mvc-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mvc-test
+//
+//  Created by Turcu Ciprian on 11/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
