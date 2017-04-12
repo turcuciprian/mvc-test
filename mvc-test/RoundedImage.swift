5b112721d415741cc69acd67d09e924774bf264d
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-12 19:04:05 +0300

    Added code from viewcontroller to a new function awakefromnib, the new rounded corner is 10 changed the UIIMageView controller image class in the identity inspector to RoundedImage

--- mvc-test/RoundedImage.swift
+++ mvc-test/RoundedImage.swift
@@ -10,12 +10,8 @@ import UIKit
 
 class RoundedImage: UIImageView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 10
     }
-    */
 
 }
