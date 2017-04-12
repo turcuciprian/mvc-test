7d5a09538ba0927816e95521bb7ddbf4524b4677
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-12 19:00:57 +0300

    created coacoa file for rounded image view

---
+++ mvc-test/RoundedImage.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedImage.swift
+//  mvc-test
+//
+//  Created by Turcu Ciprian on 12/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import UIKit
+
+class RoundedImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
